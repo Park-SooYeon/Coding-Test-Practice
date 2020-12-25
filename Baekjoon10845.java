13388889dd78637d5f3b2ec0894470de573f5d0f
수연 <cjg03194@gmail.com>
2020-12-25 21:42:49 +0900

    백준 10845 큐 풀이
    - 시간 초과 관련해서 실패가 많았던 문제..

---
+++ Baekjoon10845.java
@@ -0,0 +1,52 @@
+/*
+  문제 출처 : https://www.acmicpc.net/problem/10845
+  큐
+  푼 날짜 : 2020-12-25
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Baekjoon10845 {
+  static ArrayList<Integer> queue = new ArrayList<>();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    int commandCount;
+
+    commandCount = Integer.parseInt(br.readLine());
+
+    for(int i = 0 ; i < commandCount ; i++) {
+      String[] splitCommand = br.readLine().split(" ");
+      switch(splitCommand[0]) {
+        case "push":
+          int number = Integer.parseInt(splitCommand[1]);
+          queue.add(number);
+          break;
+        case "pop":
+          if(queue.isEmpty()) sb.append(-1 + "\n");
+          else sb.append(queue.remove(0) + "\n");
+          break;
+        case "empty":
+          if(queue.isEmpty()) sb.append(1 + "\n");
+          else sb.append(0 + "\n");
+          break;
+        case "front":
+          if(queue.isEmpty()) sb.append(-1 + "\n");
+          else sb.append(queue.get(0) + "\n");
+          break;
+        case "back":
+          if(queue.isEmpty()) sb.append(-1 + "\n");
+          else sb.append(queue.get(queue.size() - 1) + "\n");
+          break;
+        case "size":
+          sb.append(queue.size() + "\n");
+          break;
+      }
+    }
+
+    System.out.println(sb);
+  }
+}
