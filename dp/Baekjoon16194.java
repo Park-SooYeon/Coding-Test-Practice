26e43054cd6a2690236c5c1b9331d75ef00ce293
수연 <cjg03194@gmail.com>
2021-01-01 13:34:14 +0900

    백준 16194 카드 구매하기 2 풀이

---
+++ dp/Baekjoon16194.java
@@ -0,0 +1,34 @@
+package dp;
+
+/*
+  문제 출처 : https://www.acmicpc.net/problem/16194
+  카드 구매하기 2
+  푼 날짜 : 2021-01-01
+*/
+import java.util.Scanner;
+
+public class Baekjoon16194 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    int[] p = new int[n + 1];
+    int[] m = new int[n + 1];
+
+    for(int i = 1 ; i <= n ; i++) {
+      p[i] = sc.nextInt();
+    }
+
+    for(int i = 1 ; i <= n ; i++) {
+      m[i] = p[i];
+      for(int j = 1 ; j <= i ; j++) {
+        m[i] = Math.min(m[i], p[j] + m[i - j]);
+      }
+    }
+
+    System.out.println(m[n]);
+
+    sc.close();
+  }
+}
